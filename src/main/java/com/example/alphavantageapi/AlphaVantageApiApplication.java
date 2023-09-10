de9a7cbb9e1a5074ebdbac8ff732c81a72c7e8a3
Aleksandar Bajceta <aleksandarbjct@gmail.com>
2023-09-10 20:13:22 +0200

    Initial commit

---
+++ src/main/java/com/example/alphavantageapi/AlphaVantageApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.alphavantageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaVantageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaVantageApiApplication.class, args);
+	}
+
+}
